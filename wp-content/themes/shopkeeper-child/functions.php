ea527231e4b76a50371058860835ac8f76e9cf59
Aldo Lugo <aldo@aldomatic.com>
2015-09-18 14:49:26 -0500

    Initial commit

---
+++ wp-content/themes/shopkeeper-child/functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+// add_filter( 'woocommerce_loop_add_to_cart_link', 'change_add_to_cart_loop' );
+// function change_add_to_cart_loop( $product ) {
+//     global $product;
+//     return '<a href="'. esc_url( $product->get_permalink( $product->id ) ) . '">View Product</a>';
+// }
+
+?>
